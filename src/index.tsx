880730b2bc96c8666fa207f9e3baf4892e22b9a6
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-03-31 11:44:15 -0300

    Export Card component from the index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ export { default as Button } from './components/Button'
 export type { ButtonProps } from './components/Button'
 
 export { default as Card } from './components/Card'
-export type { CardProps } from './components/Card'
+export type {
+  CardProps,
+  CardContentProps,
+  CardTitleProps
+} from './components/Card'
