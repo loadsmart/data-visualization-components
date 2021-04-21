0bfb0a7da31564d8dc39954c1dfdba316ddb807d
Felipe Israel Camargo Pereira <felipe.pereira@loadsmart.com>
2021-04-21 14:54:22 -0300

    fix(circular-gauge): fix label rendering issue (#27)

---
+++ example/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>Use 'yarn start' to see the storybook.</div>
+
+ReactDOM.render(<App />, document.getElementById('root'))
