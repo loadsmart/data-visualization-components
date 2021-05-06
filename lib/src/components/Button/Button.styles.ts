3984f876c6bc569f39a56d6f80bd896df26c3098
Felipe Israel Camargo Pereira <felipe.pereira@loadsmart.com>
2021-05-06 10:44:49 -0300

    Add stylelint & Husky (#35)
    * chore(core): add stylelint and husky hooks
    
    closes #7
    
    * chore(core): add pre commit hook for stylelint

--- lib/src/components/Button/Button.styles.ts
+++ lib/src/components/Button/Button.styles.ts
@@ -3,22 +3,24 @@ import getActivatableValue from '../../utils/getActivatableValue'
 import { ButtonProps } from './Button.types'
 
 export const StyledButton = styled.button<ButtonProps>`
+  padding: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.padding')};
+
+  color: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.text.color')};
+  font-weight: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.text.weight')};
+  font-size: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.text.size')};
+  text-transform: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.text.transform')};
+
   background: ${({ theme, active }) =>
     getActivatableValue(theme, active, 'button.background')};
   border-color: ${({ theme, active }) =>
     getActivatableValue(theme, active, 'button.border.color')};
-  border-radius: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.border.radius')};
   border-width: ${({ theme, active }) =>
     getActivatableValue(theme, active, 'button.border.width')};
-  color: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.text.color')};
-  font-size: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.text.size')};
-  font-weight: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.text.weight')};
-  text-transform: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.text.transform')};
-  padding: ${({ theme, active }) =>
-    getActivatableValue(theme, active, 'button.padding')};
+  border-radius: ${({ theme, active }) =>
+    getActivatableValue(theme, active, 'button.border.radius')};
 `
