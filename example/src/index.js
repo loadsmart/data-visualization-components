1c1bec19777e4406b851a18bbaa28e89004440c0
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-05-25 16:10:00 -0300

    fix(lib): fix IE11 compatible build settings

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,15 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => <div>Use 'yarn start' to see the storybook.</div>
+import { BarChart } from '@loadsmart/data-visualization'
+
+const App = () => (
+  <div>
+    <BarChart data={[]} />
+  </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 export { default as Button } from './components/Button'
 export type { ButtonProps } from './components/Button'
 
