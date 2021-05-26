499ebed3ca5399118f58afd479088a7b4b3fe84e
Felipe Israel Camargo Pereira <felipe.pereira@loadsmart.com>
2021-05-26 10:53:21 -0300

    fix(lib): fix IE11 compatible build settings
    * fix(lib): fix IE11 compatible build settings
    
    * fix(lib): fix IE11 compatible build settings
    
    * fix(example): fix linting issues
    
    * fix(lib): fix microbundle command
    
    * ci(project): fix build command

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,20 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => <div>Use 'yarn start' to see the storybook.</div>
+import { BarChart } from '@loadsmart/data-visualization'
+
+const App = () => (
+  <div>
+    <BarChart
+      data={[
+        { name: 'Foo', value: 50 },
+        { name: 'Bar', value: 100 }
+      ]}
+    />
+  </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
